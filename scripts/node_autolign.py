258fabe22f201ba44d5bbfeed0222465b33d16f0
john <alsterda@stanford.edu>
2018-10-18 16:47:43 -0700

    try to talk to autobridge

--- scripts/node_autolign.py
+++ scripts/node_autolign.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+#from std_msgs.msg import String
+
+# This script should listen to the from_autobox auto_bridge msgs,
+#   prompt an autolign calculation,
+#   then send out a to_autobox msg
 
 def callback(data):
+    print "from_autobox:"
     print data.data
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
 def listener():
     
-    rospy.init_node('listener', anonymous+True)
+    rospy.init_node('autolign', anonymous+True)
 
     # subscribe to auto_bridge's "from_autobox" topic / String type msgs
-    rospy.Subscriber("from_autobox", String, callback)
+    rospy.Subscriber("from_autobox", from_autobox, callback)
 
     rospy.spin()
 
