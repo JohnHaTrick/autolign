ec339e6f21a2c4d7ece70c7b1f71a90a5d0d07f4
DDL <x1drivers@lists.stanford.edu>
2018-10-18 20:56:05 -0700

    subscribed to from_autobox!

--- scripts/node_autolign.py
+++ scripts/node_autolign.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import rospy
-#from std_msgs.msg import String
+from auto_bridge.msg import from_autobox
+#from auto_bridge import from_autobox
 
 # This script should listen to the from_autobox auto_bridge msgs,
 #   prompt an autolign calculation,
@@ -9,15 +10,15 @@ import rospy
 
 def callback(data):
     print "from_autobox:"
-    print data.data
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    rospy.loginfo(rospy.get_caller_id() + " I heard seq %s", str(data.header.seq))
+    print "X1 is at (%f East, %f North)" % (data.E_m,data.N_m)
 
 def listener():
     
-    rospy.init_node('autolign', anonymous+True)
+    rospy.init_node('autolign', anonymous=True)
 
-    # subscribe to auto_bridge's "from_autobox" topic / String type msgs
-    rospy.Subscriber("from_autobox", from_autobox, callback)
+    # subscribe to auto_bridge's 'from_autobox' topic, from_autobox msg type
+    rospy.Subscriber('from_autobox', from_autobox, callback)
 
     rospy.spin()
 
