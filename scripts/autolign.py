42553c327bed4758945cfd29114b8b53a879655d
John Alsterda <alsterda@stanford.edu>
2018-11-28 22:49:17 -0800

    refactor: lin-sim, sim-loop, util.py ...

--- scripts/autolign.py
+++ scripts/autolign.py
@@ -6,7 +6,8 @@ import ctrl_autolign     as ctrl
 import learn_autolign    as learn
 import matplotlib.pyplot as plt
 import fileman           as fm
-from   sim_autolign  import X1Simulator
+import sim_autolign      as sim
+import util_autolign     as util
 from   plot_autolign import Plotter
 try: 
     import ros_autolign
@@ -16,33 +17,41 @@ except:
 # High-level script for the automatic alignment of X1's wheels
 #   README to get started
 
-def simLoop(guess,i):
-    state    = sim.getState()				# current state
-    e = ctrl.calcLateralError(path,state)
-    v = state['Ux']
-    print "i: ",i," E:",round(state['E'],2)," N:",round(state['N'],2)," Psi:",round(state['psi'],2),\
-          " Ux:",round(state['Ux'],2)," Uy:",round(state['Uy'],2)," r:",round(state['r'],2)
+def simLoop(guess,i,sim_mode):
+    state    = NLsim.getState()				# current state
+    e        = ctrl.calcLateralError(path,state)
+    v        = state['Ux']
+    print "i: ",i," E: "  ,round(state['E'],2)  ," N: " ,round(state['N'],2), \
+                  " Psi: ",round(state['psi'],2)," Ux: ",round(state['Ux'],2),\
+                  " Uy: " ,round(state['Uy'],2) ," r: " ,round(state['r'],2)
     del_cmd  = ctrl.lookAheadCtrl(path,state)	        # calc steer cmds
-    #del_cmd -= guess					# apply guess (if desired)
-    Fxr      = ctrl.PI_Ctrl(path,state)			# calc Fx cmd (rear axle)
+    if sim_mode == 'val':                               # if validtating,
+        del_cmd -= guess      	                        #   apply guess
+    Fxr      = ctrl.PI_Ctrl(path,state)			# calculate Fx cmd
     print "Steer %.2f rad and throttle %.2f N" % (float(del_cmd[0]),Fxr)
     del_real = del_cmd + misalign                       # apply misalignment
-    state    = sim.simulateX1(del_real, Fxr, dt)	# simulate cmds
-    guess    = learn.gradDescent(guess, del_cmd,
-                                 state, dt, path, i)	# calc RL
-    # for plot
-    e = ctrl.calcLateralError(path,state)
-    dPsi = ctrl.calcHeadingError(path,state)
+    state    = NLsim.simulate_T(del_real, Fxr, dt)	# simulate cmds
+    if sim_mode == 'lrn':                               # if learning,
+        guess = learn.gradDescent(guess, del_cmd,
+                                  state, dt, path, i)	#   calc gradDescent
+    e = ctrl.calcLateralError(path,state)               # update storage
+    dPsi = ctrl.calcHeadingError(path,state)            #   for plotting
     state.update(delta=del_cmd, guess=guess, misalign=misalign, e=e, dPsi=dPsi)
-    print "Tru mis: [%.3f,%.3f,%.3f,%.3f]\n"%(misalign[0],misalign[1],misalign[2],misalign[3])\
-	+ "Guess:   [%.3f,%.3f,%.3f,%.3f]\n"%(guess[0],guess[1],guess[2],guess[3]) 
-    fm.write_row_csv( filename,
-      {'mis1':misalign[0],'mis2':misalign[1],'mis3':misalign[2],'mis4':misalign[3],
-        'guess1':guess[0], 'guess2':guess[1], 'guess3':guess[2], 'guess4':guess[3],
-       'delta':del_cmd[0],         'Fxr':Fxr,             'e':e,             'v':v},
-                      order=ordered )			# save data
-    #time.sleep(.01)
-    return state,np.stack(guess)
+    print "Tru mis: [%.3f,%.3f,%.3f,%.3f]\n"                \
+        % (misalign[0],misalign[1],misalign[2],misalign[3]) \
+	+ "Guess:   [%.3f,%.3f,%.3f,%.3f]\n"                \
+        % (guess[0],guess[1],guess[2],guess[3])         # status update
+    fm.write_row_csv( filename, {'mis1':misalign[0], 'mis2':misalign[1],
+                                 'mis3':misalign[2], 'mis4':misalign[3],
+                                 'guess1':guess[0],  'guess2':guess[1] ,
+                                 'guess3':guess[2],  'guess4':guess[3] ,
+                                 'delta':del_cmd[0], 'Fxr':Fxr         ,
+                                 'e':e,              'v':v             },
+                      order=ordered )                   # write data
+    state.update(t=dt*i)                                # add time to dict
+    plotter.store(state)                                # store for plotting
+    #time.sleep(.01)                                    # pause for debug?
+    return np.stack(guess)                              # end learning loop
 
 def expLoop():
     # calc cmds
@@ -51,92 +60,49 @@ def expLoop():
     # receive X1 state update
     # calc RL
     return -1
-
-def welcome(args):
-    print ("\nStarting %s" % args[0][-11:-3])   # Welcome message
-    try:                                        # Did user enter a terminal argument?
-        mode = args[1]              # choose simulate or experiment mode
-        if mode == 'sim' or mode == 'exp':
-            print ("in %s mode\n" % args[1])
-        else:
-            print ("in sim mode (by default)\n")
-            mode = 'sim'
-    except:
-        print ("in sim mode (by default)\n")
-        mode = 'sim'
-    return mode
     
-if __name__ == '__main__':			# main function
-						# Parameters and initializations:
-    mode  = welcome(sys.argv)			#   choose simulation or experiment
-    dt    = .01					#   100 msec per trial
-    T     =  4					#   10 sec experiment / simulation
-    n     = int(T/dt)				#   number of trials
-    guess = np.array([0,0,0,0])			#     to zero [FL, FR, RL, RR]
-    path  = ctrl.loadPath_debug()		#   get path
-    #path  = ctrl.loadPath_VAIL()		#   get path
+if __name__ == '__main__':			    #### main function ####
+						    # Params and inits:
+    mode  = util.welcome(sys.argv)      	    #   choose sim or exp
+    dt    = .01					    #   100 msec per trial
+    T     =  4					    #   [s] sim/exp term
+    n     = int(T/dt)				    #   num trials
+    guess = np.array([0,0,0,0])			    #   [FL, FR, RL, RR]
+    path  = ctrl.loadPath_debug()		    #   get path
+    #path  = ctrl.loadPath_VAIL()		    #   get path
 
-    if mode == 'exp':				# Mode == exp
-        ros_autolign.listener()			#   collect initial state
-        while(1):
-            expLoop()				#   run experiment loop
+    start_time = time.time()		            # init save file using
+    ordered = [ 'mis1'  , 'mis2'  , 'mis3'  , 'mis4'  ,
+                'guess1', 'guess2', 'guess3', 'guess4',
+                'delta' , 'Fxr'   , 'e'     , 'v'      ]
+    filename   = fm.initializer(order=ordered)      #   this header & timestamp
 
-    elif mode == 'sim':
-        misalign = 5*np.random.uniform(-1,1,4) \
-                 * math.pi/180			# [rad] init true misalignment
-	print "X1's simulated initial misalignment is: " + str(misalign) + ' radians\n'
-        state_0 = {'E'  : path['E']+0, # error
-		   'N'  : path['N']+0, # error
+    plotter = Plotter()                             # init Plotter class
+
+    if mode == 'exp':				    ### Mode == exp ###
+        ros_autolign.listener()			    # collect init state
+        while(1): expLoop()			    # run exp loop
+
+    elif mode == 'sim':                             ### Mode == sim ###
+        misalign = 5 * np.random.uniform(-1,1,4) \
+                     * math.pi/180		    # [rad] set true alignment
+        state_0 = {'E'  : path['E']+0, # error?
+		   'N'  : path['N']+0, # error?
 		   'psi': path['psi'],
-                   'Ux' : path['v']  , # error
+                   'Ux' : path['v']  , # error?
                    'Uy' : 0	     ,
-		   'r'  : 0
-		  }				# initialize state
-        
-        sim = X1Simulator()			# initialize sim
-        sim.setState(state_0)			# initialize sim state
-        plotter = Plotter()
+		   'r'  : 0           }		    # initialize state dict()
+        NLsim   = sim.NonlinearSimulator()	    # init NL sim class
 
-	start_time = time.time()		# create save file
-	ordered = [  'mis1',     'mis2',     'mis3',     'mis4',
-                   'guess1',   'guess2',   'guess3',   'guess4',
-                    'delta',      'Fxr',        'e',        'v']
-	filename   = fm.initializer(order=ordered)
+                                                    ## Learning Trials ##
+        NLsim.setState(state_0)			    # init NL sim state
+        for i in range(n):                          # n learning iterations
+            guess = simLoop(guess,i,'lrn')          # sim loop : learning mode
 	
-        for i in range(n):
-            state,guess = simLoop(guess,i)	# run simulation loop
-            state.update(t=dt*i)
-            plotter.store(state)
-	
-	sim.setState(state_0)			# Reset state for a Validation Trial
-	ctrl.Integrator.i = 0			# Reset longitudinal ctrl integrator
-	for j in range(n):
-	    state    = sim.getState()				# current state
-	    e = ctrl.calcLateralError(path,state)
-	    v = state['Ux']
-	    print "j: ",j,"E:",round(state['E'],2)," N:",round(state['N'],2), \
-                  " Psi:",round(state['psi'],2)," Ux:",round(state['Ux'],2), \
-                  " Uy:",round(state['Uy'],2)," r:",round(state['r'],2)
-	    del_cmd  = ctrl.lookAheadCtrl(path,state)	        # calc steer cmds
-	    del_cmd -= guess					# apply guess (if desired)
-	    Fxr      = ctrl.PI_Ctrl(path,state)			# calc Fx cmd (rear axle)
-	    print "Steer %.2f rad and throttle %.2f N" % (float(del_cmd[0]),Fxr)
-	    del_real = del_cmd + misalign			# apply misalignment
-	    state    = sim.simulateX1(del_real, Fxr, dt)	# simulate cmds
-	    # for plotting
-	    e = ctrl.calcLateralError(path,state)
-	    dPsi = ctrl.calcHeadingError(path,state)
-	    state.update(delta=del_cmd, guess=guess, misalign=misalign, e=e, dPsi=dPsi)
-	    print "Tru mis: [%.3f,%.3f,%.3f,%.3f]\n"%(misalign[0],misalign[1],
-						      misalign[2],misalign[3])\
-		+ "Guess:   [%.3f,%.3f,%.3f,%.3f]\n"%(guess[0],guess[1],
-                                                      guess[2],guess[3]) 
-	    fm.write_row_csv( filename,
-              {'mis1':misalign[0],'mis2':misalign[1],'mis3':misalign[2],'mis4':misalign[3],
-	        'guess1':guess[0], 'guess2':guess[1], 'guess3':guess[2], 'guess4':guess[3],
-	       'delta':del_cmd[0],         'Fxr':Fxr,             'e':e,             'v':v},
-	                      order=ordered )			# save data
-            state.update(t=dt*j)
-	    plotter.store(state)
-	    	    
-        plotter.plot()      # plot simulation result
+                                                    ## Validation Trials ##
+	NLsim.setState(state_0)			    # Reset sim state
+	ctrl.Integrator.i = 0			    # Reset ctrl integrator
+	for j in range(n):                          # n validation iterations
+            guess = simLoop(guess,j,'val')          # sim loop : validation mode
+	    
+        plotter.plot()                              # plot simulation result
--- scripts/sim_autolign.py
+++ scripts/sim_autolign.py
@@ -3,77 +3,53 @@
 import numpy as np
 import ctrl_autolign as ctrl
 
-# sim_autolign.py provides modelling parameters and models for X1
+# sim_autolign provides simulation models and parameters for X1
 
-def ackermann(delta_f,delta_r,d,l):
+def ackermann(delta_f,delta_r):
     # Linear model of ackermann angle
     # Small angle approx : delta_f,r,fl,fr,rl,rr << 1
     #    allows tan, tan-1 ==> 1. 
     # Input
     #   d: half length of tread
     #   l: wheel base
-    x        = d / l * ((delta_f) - (delta_r))
+    X1()
+    x        = X1.d / X1.l * ((delta_f) - (delta_r))
     delta_fl = delta_f / (1 - x)
     delta_fr = delta_f / (1 + x) 
     delta_rl = delta_r / (1 - x) 
     delta_rr = delta_r / (1 + x)
-    return delta_fl, delta_fr, delta_rl, delta_rr 
+    return [ delta_fl, delta_fr, delta_rl, delta_rr ]
 
-class X1Simulator:
-    """ Vehicle Simulation Class """
+class X1(object):
+    """ X1 Vehicle Parameter Class"""
+    M  =    1964.0                  # [kg]     Mass
+    a  =       1.50                 # [m]      Front length
+    b  =       1.37                 # [m]      Rear length  
+    l  =  a + b                     # [m]      Wheel base
+    d  =       0.82                 # [m]      Half Tread
+    Iz =    2900.0                  # [kg-m^2] Inertia 2250 = 1000*1.5*1.5
+    Cf = -140000.0                  # [N/rad]  Front Axle Cornering Stiffness 
+    Cr = -190000.0                  # [N/rad]  Rear Axle Cornering Stiffness
+    C1 =  Cf / 2.0                  # [N/rad]  Front Left Cornering Stiffness
+    C2 =  Cf / 2.0                  # [N/rad]  Front Right Cornering Stiffness
+    C3 =  Cr / 2.0                  # [N/rad]  Rear Left Cornering Stiffness
+    C4 =  Cr / 2.0                  # [N/rad]  Rear Right Cornering Stiffness
+    C  =  Cf + Cr                   # [N/rad]  Total Cornering Stiffness
+
+class NonlinearSimulator:
+    """ High Fidelity 4 Wheel Vehicle Simulation Class """
 
     def __init__(self):
-        ## Vehicle parameter    
-        self.M = 1964.0		    # Mass         [kg]
-        self.a = 1.50		    # Front length [m]
-        self.b = 1.37		    # Rear length  [m]
-        self.l = self.a+self.b      # Wheel base   [m]
-        self.d = 0.82		    # Half Tread   [m]
-        self.Iz = 2900.0	    # Inertia      [kg-m^2] 2250 = 1000*1.5*1.5
-        self.Cf = -140000.0	    # Cornering Stiffness [N/rad]
-        self.Cr = -190000.0	    # Cornering Stiffness [N/rad]
-        self.C1 = self.Cf/2.0	    # Cornering Stiffness of Front Left  [N/rad]
-        self.C2 = self.Cf/2.0	    # Cornering Stiffness of Front Right [N/rad]
-        self.C3 = self.Cr/2.0	    # Cornering Stiffness of Rear Left   [N/rad]
-        self.C4 = self.Cr/2.0	    # Cornering Stiffness of Rear Right  [N/rad]
-	self.C  = self.Cf + self.Cr
-
-
-        ## Simulation Parameter 
-        self.dt = 0.001		    # Time step [s]
-
-	# Linear model: x_dot = A*x + B*u + C
-	#   x = [ e dPsi Uy r ] ; u = [ delta_fl, delta_fr, delta_rl, delta_rr ]
-	#   see supportDocs/vehicleModels.pdf for derivation
-	
-	self.A = np.matrix([
-                            [0, Ux,      1,    0],
-                            [0, 0 ,      0,    1],
-                            [0, 0 ,-C/M/Ux, Q-Ux],
-                            [0, 0 ,   Q/Iz,    R]
-                           ])                        # dynamics
-	self.B = np.matrix([
-                            [      0,       0,       0,       0],
-                            [      0,       0,       0,       0],
-                            [   Cf/M,    Cf/M,    Cr/M,    Cr/M],
-                            [a*Cf/Iz, a*Cf/Iz,-b*Cr/Iz,-b*Cr/Iz]
-                           ])                        # control
-     C = np.matrix([
-                     0,
-                     path['k']*Ux,
-                     0,
-                     0
-                   ])                        # affine
-
-        self.east     = 0.0
+        X1()                        #     Vehicle params
+        self.dt       = 0.001	    # [s] Internal sim time step
+        self.east     = 0.0         #     Simulation-level states
         self.north    = 0.0
         self.psi      = 0.0
         self.vx       = 0.0
         self.vy       = 0.0
         self.yawrate  = 0.0
-
-        self.fx       = [0.0, 0.0, 0.0, 0.0]
-        self.delta_fr = [0.0, 0.0]
+        self.fx       = [0.0] * 4   #     Simulation commands
+        self.delta_fr = [0.0] * 2
     
     def setStateValue(self,psi,east,north,r,vx,vy):
         self.psi   = psi
@@ -89,18 +65,24 @@ class X1Simulator:
         self.fx = fx
         self.delta_fr = delta_fr
 
-    def simulate(self):
+    def simulate_dt(self):
+        # propogate self.state forward by one (small) dt
+        #   Not the same as the (larger) autolignment dt
+
         # Calc ackermann steer angle of 4 wheels, in case of bicycle commands
-        # self.delta_4ws = ackermann(self.delta_fr[0], self.delta_fr[1], self.d, self.l)
+        # self.delta_4ws = ackermann(self.del_fr, self.del_fr, self.d, self.l)
         [delta1, delta2, delta3, delta4] = self.delta_4ws
         [fx1, fx2, fx3, fx4]             = self.fx
         
         # Calc forces to each tire    
-        fy1 = self.C1 * (-delta1 + np.arctan2(self.vy + self.a * self.r, self.vx - self.d * self.r))
-        fy2 = self.C2 * (-delta2 + np.arctan2(self.vy + self.a * self.r, self.vx + self.d * self.r))
-        fy3 = self.C3 * (-delta3 + np.arctan2(self.vy - self.b * self.r, self.vx - self.d * self.r))
-        fy4 = self.C4 * (-delta4 + np.arctan2(self.vy - self.b * self.r, self.vx + self.d * self.r))
-
+        fy1 = X1.C1 * (-delta1 + np.arctan2(self.vy + X1.a * self.r,
+                                            self.vx - X1.d * self.r))
+        fy2 = X1.C2 * (-delta2 + np.arctan2(self.vy + X1.a * self.r,
+                                            self.vx + X1.d * self.r))
+        fy3 = X1.C3 * (-delta3 + np.arctan2(self.vy - X1.b * self.r,
+                                            self.vx - X1.d * self.r))
+        fy4 = X1.C4 * (-delta4 + np.arctan2(self.vy - X1.b * self.r,
+                                            self.vx + X1.d * self.r))
         fy = (fy1,fy2,fy3,fy4)
         
         f1cx = fx1 * np.cos(delta1) - fy1 * np.sin(delta1)
@@ -115,14 +97,14 @@ class X1Simulator:
         sigma2 = f1cy + f2cy + f3cy + f4cy
 
         # Calc derivatives
-        psidot = self.r
+        psidot =  self.r
         edot   = -self.vy * np.cos(self.psi) - self.vx * np.sin(self.psi)
         ndot   =  self.vx * np.cos(self.psi) - self.vy * np.sin(self.psi)
-        rdot   = 1 / self.Iz * ( self.d * ( - f1cx + f2cx - f3cx + f4cx )
-                               + self.a * (   f1cy + f2cy               )
-			       - self.b * (   f3cy + f4cy               ) )
-        vxdot  =  self.r * self.vy + 1 / self.M * sigma1
-        vydot  = -self.r * self.vx + 1 / self.M * sigma2
+        vxdot  =  self.r  * self.vy + 1 / X1.M * sigma1
+        vydot  = -self.r  * self.vx + 1 / X1.M * sigma2
+        rdot   =  1/X1.Iz * ( X1.d  * ( - f1cx + f2cx - f3cx + f4cx )
+                            + X1.a  * (   f1cy + f2cy               )
+			    - X1.b  * (   f3cy + f4cy               ) )
 
         # Calc next state
         self.psi_n   = self.psi   + self.dt * psidot 
@@ -132,52 +114,95 @@ class X1Simulator:
         self.vx_n    = self.vx    + self.dt * vxdot 
         self.vy_n    = self.vy    + self.dt * vydot 
 
+        """ IS THIS NECESSARY? - aLSTERDA"""
         # update inner value
-        self.setStateValue(self.psi_n,self.east_n,self.north_n,self.r_n,self.vx_n,self.vy_n)
+        self.setStateValue( self.psi_n, self.east_n, self.north_n,
+                            self.r_n,   self.vx_n,   self.vy_n     )
 
         # return next state
-        return [self.psi_n, self.east_n, self.north_n, self.r_n, self.vx_n, self.vy_n, fy]
+        return [ self.psi_n, self.east_n, self.north_n, 
+                 self.r_n,   self.vx_n,   self.vy_n,    fy ]
 
     def setState(self, state):
+        # set class structured state according to dictionary
         self.psi   = state['psi']
-        self.east  = state['E']
-        self.north = state['N']
-        self.vx    = state['Ux']
-        self.vy    = state['Uy']
-        self.r     = state['r']
+        self.east  = state['E'  ]
+        self.north = state['N'  ]
+        self.vx    = state['Ux' ]
+        self.vy    = state['Uy' ]
+        self.r     = state['r'  ]
 
     def getState(self):
-        # return state
-        state= {'E'    :  self.east,			# [m]   pos East. Durand Datum
-                'N'    :  self.north,			# [m]   pos North
-                'psi'  :  self.psi,			# [rad] heading
-                'Ux'   :  self.vx,			# [m/s] longitudinal speed
-                'Uy'   :  self.vy,			# [m/s] lateral speed
-                'r'    :  self.r,           # [rad/s] yawrate
-           }
+        # return state as dictionary
+        state = {'E'    : self.east ,		# [m]     pos East
+                 'N'    : self.north,		# [m]     pos North
+                 'psi'  : self.psi  ,		# [rad]   heading
+                 'Ux'   : self.vx   ,		# [m/s]   longitudinal speed
+                 'Uy'   : self.vy   ,		# [m/s]   lateral speed
+                 'r'    : self.r     }          # [rad/s] yawrate
         return state
         
-    def getLocalState(self, state, path):
-	local_state = np.array([ctrl.calcLateralError(path,memory.state_0),
-                                ctrl.calcHeadingError(path,memory.state_0),
-                                memory.state_0['Uy']                      ,
-                                memory.state_0['r']                       ])
-
-    def simulateX1(self, del_cmd, Fxr, dt):
+    def simulate_T(self, del_cmd, Fxr, T):
         # set command to simulator
         self.fx = [0,0,Fxr,Fxr]
         self.delta_4ws = del_cmd
 
         # simulate
-	for i in range(int(dt/self.dt)):
-	    self.simulate()
+	for i in range(int(T/self.dt)):
+	    self.simulate_dt()
         
         # return state
-        state= {'E'    :  self.east_n,			# [m]   pos East. Durand Datum
-                'N'    :  self.north_n,			# [m]   pos North
-                'psi'  :  self.psi_n,			# [rad] heading
-                'Ux'   :  self.vx_n,			# [m/s] longitudinal speed
-                'Uy'   :  self.vy_n,			# [m/s] lateral speed
-                'r'    :  self.r,               # [rad/s] yawrate
-           }
+        state = {'E'    : self.east_n ,		# [m]     pos East
+                 'N'    : self.north_n,	        # [m]     pos North
+                 'psi'  : self.psi_n  ,		# [rad]   heading
+                 'Ux'   : self.vx_n   ,		# [m/s]   longitudinal speed
+                 'Uy'   : self.vy_n   ,		# [m/s]   lateral speed
+                 'r'    : self.r       }        # [rad/s] yawrate
         return state
+
+class LinearSimulator:
+    """
+    Linearized 4 Wheel Vehicle Simulation Class
+      x_dot = A*x + B*u + C
+      x = [ e dPsi Uy r ] ; u = [ delta_fl, delta_fr, delta_rl, delta_rr ]
+      see supportDocs/vehicleModels.pdf for derivation
+    """
+    def __init__(self):
+        X1()
+    
+    def simulate_Lin(self, local_state_0, Ux, path, delta, dt):
+        a  = X1.a
+        b  = X1.b
+        M  = X1.M
+        Iz = X1.Iz
+        Cf = X1.Cf
+        Cr = X1.Cr
+        Q  =  (Cr*b   - Cf*a  )/M /Ux 
+        R  = -(Cf*a*a + Cr*b*b)/Iz/Ux 
+        C  =  Cf + Cr
+        I  =  np.identity(4) 
+        A  = np.matrix([ [0, Ux,      1,    0], 
+                         [0, 0 ,      0,    1], 
+                         [0, 0 ,-C/M/Ux, Q-Ux], 
+                         [0, 0 ,   Q/Iz,    R] ])                # dynamics 
+        B  = np.matrix([ [      0,       0,       0,       0], 
+                         [      0,       0,       0,       0], 
+                         [   Cf/M,    Cf/M,    Cr/M,    Cr/M], 
+                         [a*Cf/Iz, a*Cf/Iz,-b*Cr/Iz,-b*Cr/Iz] ]) # control 
+        C  = np.matrix([ 0           ,
+                         path['k']*Ux,
+                         0           ,
+                         0             ])                        # affine
+
+        local_state_1 = ( I + dt*A ).dot(local_state_0) \
+                      + dt * ( B.dot(delta) + C )
+
+        return local_state_1, B
+
+    def getLocalState(self, state, path):
+	local_state = np.array([ctrl.calcLateralError(path,state),
+                                ctrl.calcHeadingError(path,state),
+                                state['Uy']                      ,
+                                state['r']                        ])
+        return local_state
+
